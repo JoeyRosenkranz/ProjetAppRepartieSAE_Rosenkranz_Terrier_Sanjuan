772944544f818002450dc82e7403a8b5e220a839
JoeyRosenkranz <joeykaizoku@gmail.com>
2025-06-10 15:28:31 +0200

    js

---
+++ js/index.js
@@ -0,0 +1,9 @@
+
+let buttonReserver =  document.createElement("button");
+buttonReserver.className = "btn btn-primary";
+buttonReserver.innerHTML = "Réserver";
+
+let buttonListerTable = document.createElement("button");
+buttonListerTable.className = "btn btn-secondary";
+buttonListerTable.innerHTML = "Lister les tables";
+
